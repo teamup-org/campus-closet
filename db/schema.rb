a4bb2c543a1c23eda57e1aa4938577b609d7f1dc
Jakob Kirby <kirbyj3684@tamu.edu>
2024-08-08 15:16:38 -0500

    Fixed claim button size

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_22_012442) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_19_174236) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -177,8 +177,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_22_012442) do
     t.float "latitude"
     t.float "longitude"
     t.boolean "admin"
-    t.string "name"
-    t.string "password_digest"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
